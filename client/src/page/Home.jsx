ae228379221680020c5694f5c5e299e1051f83f0
Jose V <valdezjose856@gmail.com>
2023-04-10 03:19:07 -0500

    fix url

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://dall-e-rose-gamma.vercel.app/', {
+      const response = await fetch('https://dall-e-rose-gamma.vercel.app/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
